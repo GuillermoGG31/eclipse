012f46a3f53ee9571362b203ecd7c50dc82dddd9
GuillermoGG31 <guillermo.gargom.2@educa.jcyl.es>
2023-04-14 20:26:38 +0200

    Haciendo mi primer commit

---
+++ src/prueba13abrilentornos/holamundo.java
@@ -0,0 +1,9 @@
+package prueba13abrilentornos;
+public class holamundo {
+	public static void main(String[] args) {
+		System.out.println("Cambio 1");
+		System.out.println("Cambio 2");
+		System.out.println("Cambio 3");
+		System.out.println("Cambio 4");
+	}
+}
